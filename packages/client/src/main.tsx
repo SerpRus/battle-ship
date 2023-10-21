82d03fb254bcd2cfddfde721001db5f7a2c82d2b
Kazakov Michael <kazakov_my@nlmk.com>
2023-10-21 23:15:52 +0300

    feature(LT-14): styles fix

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/scss/index.scss'
+import './app/styles/scss/index.scss'
 import 'normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
