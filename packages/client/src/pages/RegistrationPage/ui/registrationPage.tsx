3df316098512e7e485e1a38d2e7abf7af3125c04
Kazakov Michael <kazakov_my@nlmk.com>
2023-10-21 23:07:18 +0300

    feature(LT-18): add validation to fields

--- packages/client/src/pages/RegistrationPage/ui/registrationPage.tsx
+++ packages/client/src/pages/RegistrationPage/ui/registrationPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Layout, Button, Checkbox, Form, Input } from 'antd'
+import { Layout, Button, Form, Input } from 'antd'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import cls from './registrationPage.module.scss'
+import {
+  EMAIL_REGEXP,
+  FIRST_NAME_REGEXP,
+  LOGIN_REGEXP,
+  PASSWORD_REGEXP,
+  PHONE_REGEXP,
+  SECOND_NAME_REGEXP,
+} from '../../../shared/constants/validationConstants'
+import { ValidatableFormItemInput } from '../../../shared/ui/ValidatableFormItemInput/ValidatableFormItemInput'
 
 const { Content } = Layout
 
@@ -18,6 +28,24 @@ type FieldType = {
 
 export const RegistrationPage = () => {
   const navigate = useNavigate()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      login: '',
+      first_name: '',
+      second_name: '',
+      phone: '',
+      username: '',
+      password: '',
+      confirm_password: '',
+    },
+  })
   const onFinish = (values: any) => {
     console.log('Success:', values)
   }
@@ -34,59 +62,132 @@ export const RegistrationPage = () => {
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
+          onFinish={handleSubmit(onFinish)}
           onFinishFailed={onFinishFailed}
           autoComplete="off">
-          <Form.Item<FieldType>
+          <ValidatableFormItemInput<FieldType>
             label="Почта"
             name="email"
-            rules={[{ required: true, message: 'Введите вашу почту' }]}>
-            <Input />
-          </Form.Item>
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Логин - обязательное поле' },
+              validate: value =>
+                EMAIL_REGEXP.test(value) || 'Введите корректную почту',
+            }}
+          />
 
-          <Form.Item<FieldType>
-            label="Логин"
+          <ValidatableFormItemInput<FieldType>
             name="login"
-            rules={[{ required: true, message: 'Введите ваш логин' }]}>
-            <Input />
-          </Form.Item>
+            label="Логин"
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Логин - обязательное поле' },
+              minLength: {
+                value: 3,
+                message: 'Логин должен быть длиннее 3 символов',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Логин должен быть короче 20 символов',
+              },
+              validate: value =>
+                LOGIN_REGEXP.test(value) ||
+                'Логин должен состоять из английских букв или спецсимволов(-_)',
+            }}
+          />
 
-          <Form.Item<FieldType>
+          <ValidatableFormItemInput<FieldType>
             label="Имя"
             name="first_name"
-            rules={[{ required: true, message: 'Введите ваше имя' }]}>
-            <Input />
-          </Form.Item>
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Имя - обязательное поле' },
+              validate: value =>
+                FIRST_NAME_REGEXP.test(value) ||
+                'Поле должно состоять только из букв, первая заглавная',
+            }}
+          />
 
-          <Form.Item<FieldType>
+          <ValidatableFormItemInput<FieldType>
             label="Фамилия"
             name="second_name"
-            rules={[{ required: true, message: 'Введите вашу фамилию' }]}>
-            <Input />
-          </Form.Item>
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Фамилия - обязательное поле' },
+              validate: value =>
+                SECOND_NAME_REGEXP.test(value) ||
+                'Поле должно состоять только из букв, первая заглавная',
+            }}
+          />
 
-          <Form.Item<FieldType>
+          <ValidatableFormItemInput<FieldType>
             label="Телефон"
             name="phone"
-            rules={[{ required: true, message: 'Введите ваш телефон' }]}>
-            <Input type="phone" />
-          </Form.Item>
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Телефон - обязательное поле' },
+              minLength: {
+                value: 10,
+                message: 'Телефон должен быть длиннее 10 символов',
+              },
+              maxLength: {
+                value: 15,
+                message: 'Телефон должен быть короче 15 символов',
+              },
+              validate: value =>
+                PHONE_REGEXP.test(value) ||
+                'Поле должно состоять только из цифр и может начинаться с +',
+            }}
+          />
 
-          <Form.Item<FieldType>
-            label="Пароль"
+          <ValidatableFormItemInput<FieldType>
             name="password"
-            rules={[{ required: true, message: 'Введите пароль' }]}>
-            <Input.Password />
-          </Form.Item>
+            label="Пароль"
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Пароль - обязательное поле' },
+              minLength: {
+                value: 8,
+                message: 'Пароль должен быть длиннее 8 символов',
+              },
+              maxLength: {
+                value: 40,
+                message: 'Пароль должен быть короче 40 символов',
+              },
+              validate: () =>
+                PASSWORD_REGEXP.test(watch('password')) ||
+                'Пароль должен содержать хотя бы одну заглавную букву или цифру, без спецсимволов',
+            }}
+            isPassword
+          />
 
-          <Form.Item<FieldType>
+          <ValidatableFormItemInput<FieldType>
             label="Повторите пароль"
             name="confirm_password"
-            rules={[{ required: true, message: 'Повторите пароль' }]}>
-            <Input.Password />
-          </Form.Item>
+            control={control}
+            errors={errors}
+            rules={{
+              required: { value: true, message: 'Пароль - обязательное поле' },
+              minLength: {
+                value: 8,
+                message: 'Пароль должен быть длиннее 8 символов',
+              },
+              maxLength: {
+                value: 40,
+                message: 'Пароль должен быть короче 40 символов',
+              },
+              validate: () =>
+                watch('password') === watch('confirm_password') ||
+                'Пароль должен cовпадать',
+            }}
+            isPassword
+          />
 
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button type="primary" htmlType="submit">
