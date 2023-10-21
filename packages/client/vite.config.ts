11fd4d0d5582de647c7baee1c4d5b9fd5b6243bd
SerpRus <Slawka00@yandex.ru>
2023-10-21 03:07:41 +0300

    lt-10 / add scss, fonts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -12,4 +12,18 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @import "./src/assets/scss/_vars.scss";
+            @import "./src/assets/scss/base/_fonts.scss";
+            @import "./src/assets/scss/base/_generic.scss";
+            @import "./src/assets/scss/base/_include.scss";
+            @import "./src/assets/scss/utils/_functions.scss";
+            @import "./src/assets/scss/utils/_mixins.scss";
+        `,
+      },
+    },
+  },
 })
