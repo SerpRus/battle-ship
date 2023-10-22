25764a2bb34cc105d2ac1ded5849820dfa8cae3f
Kazakov Michael <kazakov_my@nlmk.com>
2023-10-22 23:19:32 +0300

    feature(LT-15): add login and logout

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './app/styles/App.scss'
 import { AppRouter } from './app/providers/router/AppRouter'
+import { useAuth } from './shared/lib/hooks/useAuth'
+import { useAuthUser } from './pages/LoginPage/model/hooks/useAuthUser'
 
 function App() {
+  const checkIsAuth = useAuthUser()
+  const { auth, setIsAuth } = useAuth()
+
   useEffect(() => {
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`
@@ -14,9 +19,16 @@ function App() {
     fetchServerData()
   }, [])
 
+  useEffect(() => {
+    checkIsAuth().then(res => {
+      console.log(res, 'res')
+      setIsAuth(res)
+    })
+  }, [checkIsAuth, setIsAuth])
+
   return (
     <div className="App">
-      <AppRouter />
+      <AppRouter isAuth={auth} />
     </div>
   )
 }
--- packages/client/src/app/providers/router/Layout.tsx
+++ packages/client/src/app/providers/router/Layout.tsx
@@ -1,18 +1,36 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useCallback } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
 import Header from '../../../shared/ui/Header/Header'
 import cls from './Layout.module.scss'
+import { RoutePath } from './routeConfig'
 
 const LayoutWrapper = ({
-  authOnly = false,
+  isAuth,
   children,
 }: {
-  authOnly?: boolean
+  isAuth: boolean
   children: ReactNode
-}) => (
-  <main className={cls.container}>
-    <Header authOnly={authOnly} />
-    {children}
-  </main>
-)
+}) => {
+  const location = useLocation()
+  const returnContent = useCallback(() => {
+    if (!isAuth) {
+      return (
+        <>
+          <Navigate to={RoutePath.login} state={{ from: location }} replace />
+          {children}
+        </>
+      )
+    }
+
+    return children
+  }, [children, isAuth, location])
+
+  return (
+    <main className={cls.container}>
+      <Header isAuth={isAuth} />
+      {returnContent()}
+    </main>
+  )
+}
 
 export default LayoutWrapper
