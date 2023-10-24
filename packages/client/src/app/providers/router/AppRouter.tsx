392c505e8b7a51458e6617ca25e31063137d1273
mementocodum <119350289+mementocodum@users.noreply.github.com>
2023-10-24 09:26:10 +0300

    feature(LT-15): add login and logout (#11)
    Co-authored-by: Kazakov Michael <kazakov_my@nlmk.com>

--- packages/client/src/app/providers/router/AppRouter.tsx
+++ packages/client/src/app/providers/router/AppRouter.tsx
@@ -3,22 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AppRouteProps, routeConfig } from './routeConfig'
 import LayoutWrapper from './Layout'
 
-export const AppRouter = () => {
+export const AppRouter = ({ isAuth }: { isAuth: boolean }) => {
   const renderWithWrapper = useCallback(
     (route: AppRouteProps) => (
       <Route
         key={route.path}
         path={route.path}
-        element={
-          <LayoutWrapper authOnly={route?.authOnly}>
-            {route.element}
-          </LayoutWrapper>
-        }
+        element={<LayoutWrapper isAuth={isAuth}>{route.element}</LayoutWrapper>}
       />
     ),
-    []
+    [isAuth]
   )
-
   return (
     <Router>
       <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>
