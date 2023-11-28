cdaaf9beb941b7e023a837e384f89bd98bacbf9f
Kazakov Michael <kazakov_my@nlmk.com>
2023-11-28 09:51:26 +0300

    feature(LT-39): merge conflicts

--- packages/client/ssr.config.ts
+++ packages/client/ssr.config.ts
@@ -42,7 +42,6 @@ export default defineConfig({
         dir: 'ssr-dist',
       },
     },
-    ssr: true,
   },
   resolve: {
     alias: [
--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,11 +1,19 @@
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
+import { Provider } from 'react-redux';
 import App from './src/App';
+import { createStore } from './src/store';
+
+export { createStore };
+
+const store = createStore(undefined);
 
 export function render(url: string) {
   return renderToString(
-    <StaticRouter location={url}>
-      <App />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </Provider>
   );
 }
