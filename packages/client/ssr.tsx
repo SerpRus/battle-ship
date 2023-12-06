2f9d69b576f0ba0febea300ff53944e049ff42a0
lirioblanko <goldliliya@mail.ru>
2023-12-06 20:03:19 +0300

    LT-40: ssr

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -2,13 +2,8 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
 import App from './src/App';
-import { createStore } from './src/store';
 
-export { createStore };
-
-const store = createStore(undefined);
-
-export function render(url: string) {
+export function render(url: string, store) {
   return renderToString(
     <Provider store={store}>
       <StaticRouter location={url}>
