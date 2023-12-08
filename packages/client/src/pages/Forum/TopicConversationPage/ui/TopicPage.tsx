3065f5fbdd3507b03e7f3e2eaca89411ad5d7962
Aleksandr <shtilnuhcenberg@gmail.com>
2023-12-08 20:19:40 +0300

    Add notifications API

--- packages/client/src/pages/Forum/TopicConversationPage/ui/TopicPage.tsx
+++ packages/client/src/pages/Forum/TopicConversationPage/ui/TopicPage.tsx
@@ -1,8 +1,21 @@
-import React, { useState, useEffect, ChangeEvent, useCallback } from 'react';
+import React, {
+  useState,
+  useEffect,
+  ChangeEvent,
+  useCallback,
+  useMemo,
+} from 'react';
 import { useParams } from 'react-router-dom';
 import { Card, Layout, Flex, Button, Form, Input } from 'antd';
-import { LikeFilled, SendOutlined } from '@ant-design/icons';
-import { v4 as makeUUID } from 'uuid';
+import {
+  LikeFilled,
+  SendOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
+
+import { TUser } from '../../../ProfilePage/model/types';
+
+import { UserStore } from '../../../ProfilePage/model/store';
 
 import forumData from '../../data.json';
 import { CommentCard } from '../../../../shared/ui/CommentCard/CommentCard';
@@ -34,6 +47,23 @@ const onFinishFailed = (errorInfo: unknown) => {
   console.log('Failed:', errorInfo); // eslint-disable-line no-console
 };
 
+function notifyUser() {
+  if (!('Notification' in window)) {
+    // eslint-disable-next-line no-alert
+    alert("don't support notifications");
+  } else if (Notification.permission === 'granted') {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const notify = new Notification('Thanks for enabling notifications!');
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const notify = new Notification('Thanks for enabling notifications!');
+      }
+    });
+  }
+}
+
 export const Topic: React.FC = () => {
   const { id } = useParams();
 
@@ -44,6 +74,15 @@ export const Topic: React.FC = () => {
     []
   );
   const [inputText, setInputText] = useState('');
+
+  const [user, setUser] = useState<TUser>({} as TUser);
+  const store = useMemo(() => new UserStore(), []);
+
+  const getUserCallback = useCallback(() => {
+    store.getUser().then(userData => {
+      setUser(userData);
+    });
+  }, [store]);
 
   useEffect(() => {
     const currentTopicComments = commentsData.filter(
@@ -60,52 +99,76 @@ export const Topic: React.FC = () => {
     setInputText(element.value);
   }, []);
 
+  // const [notification, setNotification] = useState(false);
+
+  useEffect(() => {
+    getUserCallback();
+  }, [getUserCallback]);
+
   const sendData = useCallback(() => {
     if (inputText) {
       const dataCopy = [...commentsData];
-
-      dataCopy.push({
-        id: makeUUID(),
+      const newCommentData = {
+        id: user.id,
         topicId: Number(id),
-        name: `User-${makeUUID()}`,
+        name: user.display_name ?? `${user.first_name} ${user.second_name}`,
         creationDate: new Date().toString(),
         likesCount: 0,
         comment: inputText,
-      });
+      };
+      if ('Notification' in window && Notification.permission === 'granted') {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len
+        const notify = new Notification(
+          `${currentTopicData?.name} - ${newCommentData.name}: ${newCommentData.comment}`
+        );
+      }
+
+      dataCopy.push(newCommentData);
       setData(dataCopy);
       setInputText('');
     }
-  }, [commentsData, inputText, id]);
+  }, [commentsData, inputText, id, user, currentTopicData]);
 
   return (
     <Layout className={cls.layout}>
-      <Flex justify="space-around" className={cls.wrapper}>
-        <Content
-          style={{
-            maxWidth: 800,
-          }}>
-          <Card className={cls.themeCard}>
-            <h2>{currentTopicData?.name}</h2>
-            <h4>{currentTopicData?.description}</h4>
-            <div>
-              <LikeFilled />
-            </div>
-          </Card>
-          <CommentCard itemList={readyMadeComments}>
-            <Form
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off">
-              <Flex>
-                <Input value={inputText} onChange={onChange} />
-                <Button onClick={sendData}>
-                  <SendOutlined />
+      {currentTopicData && user && (
+        <Flex justify="space-around" className={cls.wrapper}>
+          <Content
+            style={{
+              maxWidth: 800,
+            }}>
+            <Card className={cls.themeCard}>
+              <h2>{currentTopicData?.name}</h2>
+              <h4>{currentTopicData?.description}</h4>
+              <div>
+                <LikeFilled />
+                <Button onClick={() => notifyUser()}>
+                  <NotificationOutlined />
                 </Button>
-              </Flex>
-            </Form>
-          </CommentCard>
-        </Content>
-      </Flex>
+              </div>
+            </Card>
+            <CommentCard itemList={readyMadeComments}>
+              <Form
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off">
+                <Flex>
+                  <Input value={inputText} onChange={onChange} />
+                  <Button onClick={sendData}>
+                    <SendOutlined />
+                  </Button>
+                </Flex>
+              </Form>
+            </CommentCard>
+          </Content>
+        </Flex>
+      )}
+      {!currentTopicData && (
+        <>
+          <h1>404</h1>
+          <h3>Not found topic/user</h3>
+        </>
+      )}
     </Layout>
   );
 };
