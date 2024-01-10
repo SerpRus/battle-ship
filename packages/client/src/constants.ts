fe4eb3081d85fdb345d20a868926d9f19f11e33c
mpankratov <mpankratov@t1-cloud.ru>
2024-01-10 14:06:43 +0300

    release

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = 'https://ya-praktikum.tech/api/v2';
-export const REDIRECT_URL = 'http://62.84.120.11/';
+export const REDIRECT_URL =
+  'http://lazy-team-battle-ship-31.ya-praktikum.tech/';
